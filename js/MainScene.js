0b22831f2b8f053b6c1a2ff527057d29ebf5a6ec
Neil M <mathew.neil@gmail.com>
2024-05-03 01:07:37 +0530

    Add static objects to map

--- js/MainScene.js
+++ js/MainScene.js
@@ -10,18 +10,19 @@ export default class MainScene extends Phaser.Scene {
     Player.preload(this);
     this.load.image('tiles', 'assets/images/map/TopDownHouse_FloorsAndWalls.png');
     this.load.tilemapTiledJSON('map', 'assets/images/map/cat-dimension.json');
+    this.load.atlas('objects', 'assets/images/objects/objects.png', 'assets/images/objects/objects_atlas.json');
   }
 
   create() {
     console.log("create");
-    
+
     const map = this.make.tilemap({ key: 'map' });
     this.map = map;
     const tileset = map.addTilesetImage('TopDownHouse_FloorsAndWalls', 'tiles', 16, 16, 0, 0);
     const layer1 = map.createStaticLayer('Tile Layer 1', tileset, 0, 0);
-    layer1.setCollisionByProperty({collide: true});
+    layer1.setCollisionByProperty({ collide: true });
     this.matter.world.convertTilemapLayer(layer1);
-    
+
     this.player = new Player({ scene: this, x: 240, y: 180, texture: 'player_lily', frame: 'cat_sleep_1' })
     this.player.inputKeys = this.input.keyboard.addKeys({
       up: Phaser.Input.Keyboard.KeyCodes.W,
@@ -29,10 +30,19 @@ export default class MainScene extends Phaser.Scene {
       left: Phaser.Input.Keyboard.KeyCodes.A,
       right: Phaser.Input.Keyboard.KeyCodes.D,
     })
+
+    this.addObjectToMap({x: 50, y: 50, objectKey: "img_fish_bowl"});
   }
 
   update() {
     console.log("update");
     this.player.update()
+  }
+
+  addObjectToMap(info){
+    let {x, y, objectKey} = info
+    let object = new Phaser.Physics.Matter.Sprite(this.matter.world, x, y, 'objects', objectKey);
+    object.setStatic(true);
+    this.add.existing(object);
   }
 }
